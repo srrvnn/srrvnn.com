07fffff88bd09de5451b53aff0cbb661cccc684a
srrvnn <srrvnn@live.com>
2014-05-14 17:34:57 -0700

    new design with angular

--- new/js/controllers.js
+++
@@ -1,9 +0,0 @@
-var myControllers = angular.module('myControllers', []);
-
-myControllers.controller('ListController', ['$scope', '$http', function ($scope, $http) {
-
-  $http.get('data/data.json').success(function (data) {
-    $scope.links = data;
-  });  
-
-}]);
