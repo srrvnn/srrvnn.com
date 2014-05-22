c283b0c8701666d9edbaf5c8568a331b0b28ef31
srrvnn <srrvnn@live.com>
2014-05-22 10:28:48 -0700

    doing now comes from rawgit.com automatically

--- js/controllers.js
+++ js/controllers.js
@@ -2,11 +2,12 @@ var myControllers = angular.module('myControllers', []);
 
 myControllers.controller('LinksController', ['$scope', '$http', function ($scope, $http) {
 
-	$http.get('data/links.json').success(function (data) {
+	$http.get('data/does.json').success(function (data) {
 	  $scope.does = data;
   });
 
-  $http.get('data/pages.json').success(function (data) {
+  $http.get('https://rawgit.com/srrvnn/d508227369c65bc95348/raw/learn.json').success(function (data) {
+
     $scope.doing = data;
   });
 
